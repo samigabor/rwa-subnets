01e58b107991be1e91527c3ad99af38d99fa8b29
samigabor <samoila.gabor@yahoo.com>
2024-04-02 16:24:07 +0300

    fix: foundry remappings

--- borrower-backend/test/Borrower.t.sol
+++ borrower-backend/test/Borrower.t.sol
@@ -2,18 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+
 import {Borrower} from "../src/Borrower.sol";
 
 contract BorrowerTest is Test {
     Borrower public borrower;
 
     function setUp() public {
-        borrower = new Borrower();
-        //counter.setNumber(0);
+        borrower = new Borrower(msg.sender);
     }
 
     function test_loanRequest() public {
-        //Borrower.increment();
         //assertEq(counter.number(), 1);
     }
 
